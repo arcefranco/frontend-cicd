e55ada4ac1eb25c1a652e0931a1f14713d15d9e5
franco <2>
2023-04-14 14:38:54 -0300

    update front

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_BACKEND_URL}`)
+      .then((response) => setData(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  const getOtra = () => {
+    return axios
+      .get(`${process.env.REACT_APP_BACKEND_URL}/otra`)
+      .then((response) => setData(response.data))
+      .catch((error) => console.log(error));
+  };
   return (
     <div className="App">
       <h1>hola frontend</h1>
+      <button onClick={getOtra}>Otra</button>
+      {data &&
+        data.map((e) => {
+          return <p>{e.name}</p>;
+        })}
     </div>
   );
 }
