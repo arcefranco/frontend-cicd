e7fdb04efff42cc4d79588cffe7869b0aaa5cacf
franco <2>
2023-04-18 13:04:51 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-require("dotenv").config();
+
 function App() {
   const [data, setData] = useState([]);
 
@@ -11,7 +11,6 @@ function App() {
       .then((response) => setData(response.data))
       .catch((error) => console.log(error));
     console.log("env: ", process.env);
-    console.log(env);
   }, []);
 
   const getOtra = () => {
